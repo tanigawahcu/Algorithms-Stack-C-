29eb2a4eabe5425981211a9eb449b6b54a576915
tanigawahcu <tanigawa@hiroshima-cu.ac.jp>
2020-09-15 11:02:17 +0900

    main.c is added

---
+++ main.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<libstack.h>
+
+void main() {
+    struct cell * init=NULL;
+    int x;
+
+    /* stackの状態を表示 */
+    printstack(init);
+
+    /* stackに2をpushして，更新後のstackへのポインタを受け取る */
+    printf("push(init, 2)\n");
+    init = push(init, 2);
+    printstack(init); // stackの状態を表示
+
+    /* stackに4をpushして，更新後のstackへのポインタを受け取る */
+    printf("push(init, 4)\n");
+    init = push(init, 4);
+    printstack(init); // stackの状態を表示
+
+    /* stackに1をpushして，更新後のstackへのポインタを受け取る */
+    printf("push(init, 1)\n");
+    init = push(init, 1);
+    printstack(init); // stackの状態を表示
+
+    /* stackからpopして，popされた値を表示する */
+    printf("pop(init)\n");
+    x = top(init);
+    print_x(x);
+    init = pop(init);
+    printstack(init);
+
+    /* stackからpopして，popされた値を表示する */
+    printf("pop(init)\n");
+    x = top(init);
+    print_x(x);
+    init = pop(init);
+    printstack(init);
+
+    /* stackからpopして，popされた値を表示する */
+    printf("pop(init)\n");
+    x = top(init);
+    print_x(x);
+    init = pop(init);
+    printstack(init);
+
+    /* 終了 */
+    return;
+}
